99493a0658afa381d7b21e53d7c884aac5dbc645
Felipe Quiroz <felipe.i.quiroz.lara@gmail.com>
2021-05-30 10:17:33 -0400

    Mejoras en products cards

--- js/getProductos.js
+++ js/getProductos.js
@@ -1,15 +1,15 @@
-$(document).ready(()=>{
+$(document).ready(() => {
 
     var seccion = null;
 
-    if (document.getElementById('mujer')){
+    if (document.getElementById('mujer')) {
         console.log('sii');
         seccion = `women's%20clothing`;
-    } else if (document.getElementById('hombre')){
+    } else if (document.getElementById('hombre')) {
         console.log('Siix2');
         seccion = `men's%20clothing`;
     }
-    
+
     var dataResult = null;
 
     $.ajax({
@@ -21,34 +21,24 @@ $(document).ready(()=>{
         mostrarProductos();
 
     });
-    
+
     const tarjetaProducto = (titulo, imgs, precio, description, id) => {
         let html = `
-        <div class="card">
-            <div class="row g-0" >
-                <div class="col-md-4 mx-auto my-auto">
-                    <img src="${imgs}" alt="..." class="img-fluid p-1">
-                </div>
-                <div class="col-md-8">
+            <div class="card h-100 shadow">
+                <img src="${imgs}" class="card-img-top p-3"/>
                 <div class="card-body">
-                    <h5 class="card-title" id="titulo">${titulo}</h5>
-                    <p class="card-text" id="descripcion">${description}</p>
-                    
-                        <hr/>                    
-                        <div class="d-flex flex-row justify-content-end">
-                            <p class="card-text text-end mx-1" id='precio'><strong>$ ${precio}</strong></p>
-                            <button class='btn btn-primary mx-2' id='${id}'>Agregar</button>
-                        </div>
-                    
+                    <h5 class="card-title">${titulo}</h5>
                 </div>
-                </div>
-            </div> 
-        </div>
+                <div class="card-footer mx-auto">
+                    <p class="card-text ">$ ${precio}</p>
+                </div>    
+            </div>
         `;
         let card = document.createElement('div');
         $(card).html(html);
-        $(card).addClass('col m-1');
+        $(card).addClass('col mt-3');
         $(card).attr('id', id);
+        
 
         return card;
     }
