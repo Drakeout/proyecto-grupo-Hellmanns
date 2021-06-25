35305a112538cc7c3f8cb7b63358981d81f7605c
Felipe Quiroz <felipe.i.quiroz.lara@gmail.com>
2021-06-25 12:44:26 -0400

    Conexión a base de datos, instalación de django y cx_oracle.
    Archivo de usuario y requerimientos están en la carpeta del proyecto django en txt

--- changeWear/changeWear/settings.py
+++ changeWear/changeWear/settings.py
@@ -78,9 +78,16 @@ WSGI_APPLICATION = 'changeWear.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': '127.0.0.1:1521/xe',
+        'USER': 'c##change_wear',
+        'PASSWORD': 'change_wear123',
+        'TEST': {
+            'USER': 'default_test',
+            'TBLSPACE': 'default_test_tbls',
+            'TBLSPACE_TMP': 'default_test_tbls_tmp',
+        },
+    },
 }
 
 
